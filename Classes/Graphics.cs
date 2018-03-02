beca5009fbb6970df91d2bb8391c41c74cfa0167
nikminer <nikminerfeed@gmail.com>
2018-03-02 23:56:56 +0300

    V. 0.5

---
+++ Classes/Graphics.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Post_Machine_Interpritator.Classes
+{
+    public class Graphics
+    {
+
+        static void drawTable(Canvas canvas, int len)
+        {
+            Line lineU = new Line()
+            {
+                Stroke = Brushes.Black,
+                X1 = 10,
+                X2 = canvas.Width - 20,
+                Y1 = 10,
+                Y2 = 10
+            };
+            canvas.Children.Add(lineU);
+            Line lineD = new Line()
+            {
+                Stroke = Brushes.Black,
+                X1 = 10,
+                X2 = canvas.Width - 20,
+                Y1 = canvas.Height - 50,
+                Y2 = canvas.Height - 50
+            };
+            canvas.Children.Add(lineD);
+
+            for (int i = 0; i != len + 1; i++)
+            {
+                Line line = new Line()
+                {
+                    Stroke = Brushes.Black,
+                    X1 = 10 + 50 * i,
+                    X2 = 10 + 50 * i,
+                    Y1 = 10,
+                    Y2 = canvas.Height - 50
+                };
+                canvas.Children.Add(line);
+            }
+        }
+        static void drawMark(Canvas canvas, Dictionary<int, int> listcells)
+        {
+            try
+            {
+                foreach (KeyValuePair<int, int> item in listcells)
+                {
+                    if (item.Value == 1)
+                    {
+                        Line linel = new Line()
+                        {
+                            StrokeThickness = 5,
+                            Stroke = Brushes.Red,
+                            X1 = 20 + 50 * item.Key,
+                            X2 = 35 + 50 * item.Key,
+                            Y1 = 25,
+                            Y2 = canvas.Height - 70
+                        };
+                        canvas.Children.Add(linel);
+                        Line liner = new Line()
+                        {
+                            StrokeThickness = 5,
+                            Stroke = Brushes.Red,
+                            X1 = 50 + 50 * item.Key,
+                            X2 = 35 + 50 * item.Key,
+                            Y1 = 25,
+                            Y2 = canvas.Height - 70
+                        };
+                        canvas.Children.Add(liner);
+                    }
+                }
+            }
+            catch { }
+        }
+        static void drawKarret(Canvas canvas,int karretpos)
+        {
+           
+            try
+            {
+                PointCollection collection = new PointCollection();
+                collection.Add(new Point(20 + 50 * karretpos, canvas.Height - 10));
+                collection.Add(new Point(50 + 50 * karretpos, canvas.Height - 10));
+                collection.Add(new Point(35 + 50 * karretpos, canvas.Height - 45));
+                Polygon tangle = new Polygon()
+                {
+                    Fill = Brushes.Black,
+                    Points = collection
+
+                };
+                canvas.Children.Add(tangle);
+            }
+            catch { }
+        }
+        public static void drawAll(Canvas canvas, Dictionary<int,int> listcells,int karretpos, int len)
+        {
+            canvas.Children.RemoveRange(0, canvas.Children.Count);//удалить всё
+            drawTable(canvas, len);
+            drawMark(canvas,listcells);
+            drawKarret(canvas, karretpos);
+        }
+    }
+}
---
+++ Creat0r.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using Post_Machine_Interpritator.Classes;
+
+namespace Post_Machine_Intepretator
+{
+    /// <summary>
+    /// Логика взаимодействия для Creat0r.xaml
+    /// </summary>
+    public partial class Creat0r : Window
+    {
+        public Creat0r()
+        {
+            InitializeComponent();
+        }
+        public int len = 5;
+        public int karretpos = 0;
+        public Dictionary<int, int> listcells;
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Graphics.drawAll(canvas, listcells, karretpos, len);//нарисовать всё заново
+
+            try
+            {
+                ArrLen.Content = Math.Round(e.NewValue);
+                if (Math.Round(e.OldValue) < Math.Round(e.NewValue))
+                    for (int i = (int)Math.Round(e.OldValue); i < Math.Round(e.NewValue); listcells.Add(i++, 0)) ;
+                else
+                    for (int i = (int)Math.Round(e.NewValue); i < Math.Round(e.OldValue); listcells.Remove(i++)) ;
+            }
+            catch { }
+            len = (int)Math.Round(e.NewValue);
+            canvas.Width = 50 * len + 30;
+        }
+        private void canvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            listcells = new Dictionary<int, int>();
+            for (int i = 0; i < len; listcells.Add(i++, 0)) ;
+
+
+            canvas.Width = 50 * len + 30;
+            Graphics.drawAll(canvas, listcells, karretpos, len);//нарисовать всё заново
+        }
+        private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                int index = (int)Math.Floor((e.GetPosition(canvas).X - 10) / 50);
+                if (listcells.ElementAt(index).Value == 0)
+                {
+                    listcells.Remove(index);
+                    listcells.Add(index, 1);
+                }
+                else
+                {
+                    listcells.Remove(index);
+                    listcells.Add(index, 0);
+                }
+
+
+                Graphics.drawAll(canvas, listcells, karretpos, len);//нарисовать всё заново
+            }
+            catch { }
+        }
+        private void canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            karretpos = (int)Math.Floor((e.GetPosition(canvas).X - 10) / 50);
+            if (karretpos < 0) karretpos = 0;
+            if (karretpos > len - 1) karretpos = len - 1;
+            Graphics.drawAll(canvas, listcells, karretpos, len);//нарисовать всё заново
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
